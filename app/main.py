25d8bcabb417b2ccacb8669121fc6a0dae9ed5f8
yanruijie902136 <yanruijie902136@gmail.com>
2025-05-20 12:36:52 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 import sys
+from typing import NoReturn
+
+
+def execute_exit(arguments: list[str]) -> NoReturn:
+    sys.exit(int(arguments[1]))
+
+
+def execute(arguments: list[str]) -> None:
+    if not arguments:
+        return
+
+    command_name = arguments[0]
+    match command_name:
+        case "exit":
+            execute_exit(arguments)
+        case _:
+            sys.stderr.write(f"{command_name}: command not found\n")
 
 
 def main() -> None:
     while True:
         sys.stdout.write("$ ")
-        line = input()
-        sys.stderr.write(f"{line}: command not found\n")
+        arguments = input().split()
+        execute(arguments)
 
 
 if __name__ == "__main__":
