d10a4e40982565ff23d1a4a166098debc5840b50
yanruijie902136 <yanruijie902136@gmail.com>
2025-05-20 12:32:10 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 import sys
 
 
-def main():
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-
-    # Wait for user input
+def main() -> None:
+    sys.stdout.write("$ ")
     input()
 
 
